eb75346d73ee43c6b9fc7c29cdb7fa214a0f73db
Teddy Juanda <tedyzhu@gmail.com>
2025-10-08 09:24:17 +0700

    update tedy

--- application/controllers/C_divisi.php
+++ application/controllers/C_divisi.php
@@ -21,44 +21,35 @@ class C_divisi extends CI_Controller
 	{
 		$start  = $this->input->post('start') ?? 0;
 		$length = $this->input->post('length') ?? 10;
-
 		$search_input = $this->input->post('search');
 		$search = isset($search_input['value']) ? $search_input['value'] : '';
-
 		$order_input = $this->input->post('order');
 		$order_col = isset($order_input[0]['column']) ? $order_input[0]['column'] : 0;
 		$dir = isset($order_input[0]['dir']) ? $order_input[0]['dir'] : 'asc';
-
 		$columns = ['code_company', 'company_name', 'code_divisi', 'name','action'];
 		$order_by = $columns[$order_col] ?? 'name';
-
 		$data = $this->M_divisi->get_paginated_divisi($length, $start, $search, $order_by, $dir);
 		$total_records = $this->M_divisi->count_all_divisi();
 		$total_filtered = $this->M_divisi->count_filtered_divisi($search);
-
 		$url_edit   = 'C_divisi/editform/';
 		$url_delete = 'C_divisi/hapusdata/';
 		$load_grid  = 'C_divisi/griddata';
-
 		$result = [];
 		foreach ($data as $row) {
-			
-
 			$aksi = '<div class="dropdown">
-            <button type="button" class="btn btn-white btn-sm" id="aksi-dropdown-' . $row->code_divisi . '" data-bs-toggle="dropdown" aria-expanded="false">
-                More <i class="bi-chevron-down ms-1"></i>
-            </button>
-            <div class="dropdown-menu dropdown-menu-sm dropdown-menu-end" aria-labelledby="aksi-dropdown-' . $row->code_divisi . '">
-                <button class="dropdown-item editbtn" onclick="editform(\'' . $url_edit . '\', \'' . $row->uuid . '\')">
-                    <i class="bi bi-pen"></i> Edit
-                </button>
-                <div class="dropdown-divider"></div>
-                <button class="dropdown-item text-danger" onclick="hapus(\'' . $row->uuid . '\', \'' . $url_delete . '\', \'' . $load_grid . '\')">
-                    <i class="bi bi-trash3"></i> Delete
-                </button>
-            </div>
-        </div>';
-
+				<button type="button" class="btn btn-white btn-sm" id="aksi-dropdown-' . $row->code_divisi . '" data-bs-toggle="dropdown" aria-expanded="false">
+					More <i class="bi-chevron-down ms-1"></i>
+				</button>
+				<div class="dropdown-menu dropdown-menu-sm dropdown-menu-end" aria-labelledby="aksi-dropdown-' . $row->code_divisi . '">
+					<button class="dropdown-item editbtn" onclick="editform(\'' . $url_edit . '\', \'' . $row->uuid . '\')">
+						<i class="bi bi-pen"></i> Edit
+					</button>
+					<div class="dropdown-divider"></div>
+					<button class="dropdown-item text-danger" onclick="hapus(\'' . $row->uuid . '\', \'' . $url_delete . '\', \'' . $load_grid . '\')">
+						<i class="bi bi-trash3"></i> Delete
+					</button>
+				</div>
+			</div>';
 			$result[] = [
 				$row->code_company . ' - ' . $row->company_name,
 				$row->code_divisi,
@@ -67,7 +58,6 @@ class C_divisi extends CI_Controller
 				$aksi,
 			];
 		}
-
 		echo json_encode([
 			"draw" => intval($this->input->post('draw')) ?? 1,
 			"recordsTotal" => $total_records,
@@ -75,7 +65,6 @@ class C_divisi extends CI_Controller
 			"data" => $result
 		]);
 	}
-
 	function add()
 	{
 		$data['judul']     = "Form Tambah Divisi";
@@ -90,7 +79,6 @@ class C_divisi extends CI_Controller
 		$this->form_validation->set_rules('kode_divisi', 'Code Divisi', 'required');
 		$this->form_validation->set_rules('nama_divisi', 'Nama Divisi', 'required');
 		$this->form_validation->set_rules('alias', 'Nama Alias', 'required');
-
 		if ($this->form_validation->run() == FALSE) {
 			// Jika validasi gagal
 			$jsonmsg = [
@@ -100,13 +88,11 @@ class C_divisi extends CI_Controller
 			echo json_encode($jsonmsg);
 			return;
 		}
-
 		// Ambil data dari request
 		$perusahaan  = $this->input->post('perusahaan');
 		$code_divisi = $this->input->post('kode_divisi');
 		$nama_divisi = $this->input->post('nama_divisi');
 		$alias       = $this->input->post('alias');
-
 		// Cek apakah kode Divisi sudah ada
 		$param_kode =[
 			'code_divisi'  => $code_divisi,
@@ -134,7 +120,6 @@ class C_divisi extends CI_Controller
 			echo json_encode($jsonmsg);
 			exit;
 		} 
-		
 		// Data untuk insert ke database
 		$datainsert = [
 			'uuid'         => $this->uuid->v4(),
@@ -145,7 +130,6 @@ class C_divisi extends CI_Controller
 			'created_at'   => date('Y-m-d H:i:s'),
 			'updated_at'   => date('Y-m-d H:i:s')
 		];
-
 		// Melakukan insert data
 		$this->db->insert('divisions', $datainsert);
 		if ($this->db->affected_rows() > 0) {
@@ -188,7 +172,6 @@ class C_divisi extends CI_Controller
 		$code_divisi = $this->input->post('kode_divisi');
 		$nama_divisi = $this->input->post('nama_divisi');
 		$alias_post       = $this->input->post('alias');
-
 		// Cek apakah UUID Divisi ada di database
 		$data =  $this->M_divisi->get_where_divisi(['a.uuid' => $uuid])->row();
 		if ($data != null) {
@@ -226,8 +209,6 @@ class C_divisi extends CI_Controller
 					$p_alias = 'TTIDAK_LOLOS';
 				}
 			}
-			
-			
 			if($p_kode == 'LOLOS' && $p_nama == 'LOLOS' && $p_alias == 'LOLOS'){
 				// Siapkan data yang akan diupdate
 				$dataupdate = [
@@ -238,7 +219,6 @@ class C_divisi extends CI_Controller
 					'code_company' => $perusahaan,
 					'updated_at'   => date('Y-m-d H:i:s')
 				];
-	
 				// Melakukan update data
 				$update = $this->M_global->update($dataupdate, 'divisions', ['uuid' => $uuid]);
 				if ($update) {
@@ -269,7 +249,6 @@ class C_divisi extends CI_Controller
 	public function hapusdata()
 	{
 		$uuid = $this->input->post('uuid');
-
 		// Validasi input
 		if (empty($uuid)) {
 			echo json_encode([
@@ -278,15 +257,12 @@ class C_divisi extends CI_Controller
 			]);
 			return;
 		}
-
 		// Mulai transaksi
 		$this->db->trans_begin();
-
 		try {
 			// Ambil data divisi berdasarkan UUID
 			$param_kode = ['a.uuid' => $uuid];
 			$divisi = $this->M_divisi->get_where_divisi($param_kode)->row();
-
 			// Jika data tidak ditemukan
 			if (!$divisi) {
 				$this->db->trans_rollback();
@@ -298,7 +274,6 @@ class C_divisi extends CI_Controller
 			}
 			// Lakukan penghapusan data di tabel divisions
 			$this->db->where('uuid', $uuid)->delete('divisions');
-
 			if ($this->db->affected_rows() <= 0) {
 				$this->db->trans_rollback();
 				echo json_encode([
